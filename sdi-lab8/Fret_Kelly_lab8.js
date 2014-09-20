becac6b0f6bf391131304c5ba64d50af6104d354
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-20 15:06:27 -0400

    Commit 13 Updated - Lab 7 - JS Objects Project.

---
+++ sdi-lab8/Fret_Kelly_lab8.js
@@ -0,0 +1,27 @@
+//alert("Lab 8 - Sting Objects, Properties and Methods - JavaScript works! - 20 September 2014");
+
+
+// Kelly L. Fret
+// SDI 1409
+// Lab 8 - String Objects
+// 20 September 2014
+
+
+var userEmail = "klfret@fullsail.edu";
+
+var emailAddr = function(email) {
+	
+	console.log(email);               //************
+	
+	returnChar = email.indexOf("@");
+	console.log(firstChar);         //*************
+	
+	
+	
+	
+	
+};
+
+
+emailAddr(userEmail);
+
